2ce508e14226e60fa78f043e0069ef7c36c566b3
calgarybino <binoandemeskel1234@gmail.com>
2023-06-17 14:17:07 -0400

    update with adding short URL and deleting also

--- express_server.js
+++ express_server.js
@@ -1,3 +1,15 @@
+function generateRandomString() {
+  const characters =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  let randomString = "";
+
+  for (let i = 0; i < 6; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    randomString += characters[randomIndex];
+  }
+
+  return randomString;
+}
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -6,11 +18,14 @@ const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
 };
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  //res.send("Hello!");
-  res.render("urls_index", templateVars);
+   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
@@ -19,19 +34,28 @@ app.get("/urls/:id", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
+app.post("/urls", (req, res) => {
+  const shortURL = generateRandomString(); //generate a unique short URL ID
+  const longURL = req.body.longURL;
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
-// //********** */
-// app.get("/set", (req, res) => {
-//   const a = 1;
-//   res.send(`a = ${a}`);
-// });
+  urlDatabase[shortURL] = longURL; //add the short URL to long URL to the db
+  // res.redirect(`/url/${shortURL}`);
+  res.redirect("/urls");
+});
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+  const newURL = req.body.newURL;
 
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
+  urlDatabase[id] = newURL; // update the long URL in the db
+  res.redirect("/urls");
+});
+
+app.post("/urls/:id", (req, res) => {
+  const id = req.params.id;
+
+  delete urlDatabase[id]; // remove the URL from the db
+  res.redirect("/urls");
+});
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
