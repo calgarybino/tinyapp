c624c9cb311e2e890e9ef19eaff2c6a776da3979
calgarybino <binoandemeskel1234@gmail.com>
2023-06-17 14:35:55 -0400

    edited the the author name and the script

--- express_server.js
+++ express_server.js
@@ -34,6 +34,10 @@ app.get("/urls/:id", (req, res) => {
   };
   res.render("urls_show", templateVars);
 });
+// app.post("/urls", (req, res) => {
+//   console.log(req.body); // Log the POST request body to the console
+//   res.send("Ok"); // Respond with 'Ok' (we will replace this)
+// });
 app.post("/urls", (req, res) => {
   const shortURL = generateRandomString(); //generate a unique short URL ID
   const longURL = req.body.longURL;
