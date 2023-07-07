157f306dd5d07d7fcf6fd1a150f67ea7113e25b8
calgarybino <binoandemeskel1234@gmail.com>
2023-07-06 20:32:45 -0400

    I modify the route position

--- express_server.js
+++ express_server.js
@@ -12,15 +12,15 @@ app.get("/urls", (req, res) => {
   const template = { urls: urlDatabase };
   res.render("urls_index", template);
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
 app.get("/urls/:id", (req, res) => {
   const templateVars = {
     id: req.params.id,
     longURL: urlDatabase[req.params.id],
   };
   res.render("urls_show", templateVars);
-});
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
 });
 
 app.listen(PORT, () => {
